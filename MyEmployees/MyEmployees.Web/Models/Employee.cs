71054551c995323c3964f71e38cb8e8208830798
Omar Hadary <Omar Hadary>
2017-03-26 15:34:27 -0700

    created MVC and WEB API controller

---
+++ MyEmployees/MyEmployees.Web/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEmployees.Web.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
